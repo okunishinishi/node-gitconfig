5986a523945c182fe7c184cba22a171c9d001259
okunishinishi <okunishinishi@gmail.com>
2018-07-23 10:10:05 +0900

    Version incremented to 2.0.7

---
+++ test/fetchRepo_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for fetchRepo.
+ * Runs with mocha.
+ */
+'use strict'
+
+const fetchRepo = require('../lib/fetchRepo.js')
+const assert = require('assert')
+
+describe('fetch-repo', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Fetch repo', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
